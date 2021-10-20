8b64ba9241abb7720a695ffe2db6d88c355ca533
bujowskis <s.bujowski@gmail.com>
2021-10-20 16:18:08 +0200

    start work on Wallet class

--- classes/Wallet.cpp
+++ classes/Wallet.cpp
@@ -1,4 +1,31 @@
 #include "Wallet.h"
 
 
-// todo
+/*
+ * Constructor of Wallet. Parameters:
+ * string owner - name of the Wallet's owner
+ * Currency *currencies - list of all available currencies
+ * Currency *main_currency - currency with exchange_rate = 1; i.e. used as anchor point to buy other currencies with
+ *      and to sell other currencies for
+ */
+// todo - what's "noexcept" here for?
+Wallet::Wallet(string owner, Currency *currencies, Currency *main_currency) noexcept {
+    this->owner = owner;
+    this->currencies = currencies;
+    this->main_currency = main_currency;
+}
+
+/* Starts a loop over all currencies in the wallet, invoking currency.trade() */
+void Wallet::tradeAll() {
+    // todo
+}
+
+/* Lists most important information of all currencies in the wallet */
+void Wallet::listAll() {
+    // todo
+}
+
+/* Prints detailed information of all currencies in the wallet */
+void Wallet::toString() {
+    // todo
+}
--- classes/Wallet.h
+++ classes/Wallet.h
@@ -1,9 +1,21 @@
+#include <string>
+#include "abstract/Currency.h"
+
+
 #ifndef WALLET_H
 #define WALLET_H
 
 
 class Wallet {
-    // todo
+private:
+    string owner;
+    Currency *currencies;
+    Currency *main_currency;
+
+public:
+    void tradeAll();
+    void listAll();
+    void toString();
 };
 
 
